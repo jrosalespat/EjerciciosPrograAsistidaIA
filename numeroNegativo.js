3a4c4fe4769154b8ffb41e82fd34ab2af748d377
jrosalespat <jrosalespat@gmail.com>
2026-05-28 20:53:26 -0600

    agregado archivo practicaElseIf

---
+++ numeroNegativo.js
@@ -0,0 +1,7 @@
+//misma logica que en mayor edad.js pero para saber si un numero es negativo
+let numero = 3;
+if (numero <0) {
+ console.log ("Numero negativo");   
+} else {
+    console.log ("Numero positivo");
+}
---
+++ practicaElseIf.js
@@ -0,0 +1,10 @@
+//un ejemplo de else if
+let calificacion = 85;
+
+if (calificacion >= 90) {
+    console.log("Excelente");
+} else if (calificacion >= 70) {
+    console.log("Buen trabajo");
+} else {
+    console.log("Necesitas mejorar");
+}
